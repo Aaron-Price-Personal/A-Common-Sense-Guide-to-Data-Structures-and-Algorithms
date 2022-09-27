54a08650eba5371137fd44d4902c2c4a68ccbe77
Aaron Price <aaronprice12321@gmail.com>
2022-09-27 19:41:24 +0100

    Auto correct feature

--- Chap 17 - Tries/AutoComplete.py
+++ Chap 17 - Tries/AutoComplete.py
@@ -56,3 +56,37 @@ class Trie():
         if not currentNode:
             return None
         return self.collectAllWords(currentNode)
+    
+    def traverse(self,node=None):
+        current_node = node or self.root
+
+        for key, childNode in current_node.children.items():
+            print(key)
+            if key != "*":
+                self.traverse(childNode)
+    
+    def autocorrect(self, word):
+        currentNode = self.root
+
+        # Keep track of how much of the user's word we have found
+        wordFoundSoFar = ""
+
+        for char in word:
+            # If the current node has child key with current char
+            if currentNode.children.get(char):
+                wordFoundSoFar += char
+                # Follow child node
+                currentNode = currentNode.children.get(char)
+
+            else:
+                # If current char not found
+                # collect all suffixes wit hte prefix weve found
+                # Concatenate word foudn so far with ending of valid word
+                # to suggest the wrod the user meant to type in
+                return wordFoundSoFar + self.collectAllWords(currentNode)[0]
+        
+        # If the user's word is found in the trie
+        return word
+
+
+
